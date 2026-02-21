9e4bc053063935cb460d58ba1c1b6834221a5b7c
lucasjanta <lucastyle2014@gmail.com>
2026-02-21 00:52:30 -0300

    criando cena de tutorial e adicionando fontes

---
+++ scripts/actors/player.gd
@@ -0,0 +1,13 @@
+extends CharacterBody2D
+
+@export var speed := 250.0
+@export var jump_force := -400.0
+@export var gravity_force := 900.0
+
+
+func _physics_process(delta: float) -> void:
+	# Adiciona gravidade
+	if not is_on_floor():
+		velocity.y += gravity_force * delta
+
+	move_and_slide()
